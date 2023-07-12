2678bd3af508185e9b616fc0be3b5505eddd1ae6
Tanya M <130961406+sciencehas@users.noreply.github.com>
2023-07-12 10:40:55 -0400

    Add files via upload

--- cheeseandrice-05ddd7b526937cda92458f414204ec128847b0fd/cheeseandrice-05ddd7b526937cda92458f414204ec128847b0fd/project_root/manage.py
+++ cheeseandrice-05ddd7b526937cda92458f414204ec128847b0fd/cheeseandrice-05ddd7b526937cda92458f414204ec128847b0fd/project_root/manage.py
@@ -1,13 +1,12 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+# manage.py
+
 import os
 import sys
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_root.settings')
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_root.settings")
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import project
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -15,6 +14,3 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-if __name__ == '__main__':
-    main()
